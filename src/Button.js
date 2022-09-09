de40a19c0d336150b9822a457227bb39976fcd13
Akhil-Kulal <akhilkulal9@gmail.com>
2022-09-09 10:18:27 +0530

    Add functionalities to show tooltip in selected direction

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,50 @@
 import React from "react";
+import "./assets/App.css";
 import "./assets/Button.css";
 
 class Button extends React.Component {
+  constructor(props) {
+    super();
+
+    // setting flag as false initially
+    this.state = {
+      flag: false,
+    };
+
+    // binding the current instant with the function call
+    this.handleMouseIn = this.handleMouseIn.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  // this function is called when mouse is hovered over the button
+  handleMouseIn() {
+    this.setState({ flag: true });
+  }
+
+  // this function is called when mouse is unhovered from the button
+  handleMouseOut() {
+    this.setState({ flag: false });
+  }
+
   render() {
+    // if flag is true, display->block otherwise none
+    const toolTipVisibility = { display: this.state.flag ? "block" : "none" };
+
+    // fetching position from props
+    const { position } = this.props;
+
     return (
-      <div>
-        <button>Hover over me</button>
+      <div className="App">
+        <button
+          onMouseOver={this.handleMouseIn}
+          onMouseOut={this.handleMouseOut}
+        >
+          <span>Hover over me</span>
+          {/* div for tooltip box */}
+          <div className={position} style={toolTipVisibility}>
+            Thanks for hovering !
+          </div>
+        </button>
       </div>
     );
   }
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,19 +1,39 @@
 import React from "react";
+import "./assets/App.css";
 import "./assets/Dropdown.css";
 import Button from "./Button";
 
 class Dropdown extends React.Component {
+  constructor() {
+    super();
+
+    // setting initial state to top
+    this.state = {
+      position: "top",
+    };
+  }
+
+  // this function is called when user select any position from dropdown
+  handleChange = (e) => {
+    // setting the position after fetching from event target value
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         {/* Dropdown List */}
-        <select className="drop-down">
+        <select className="drop-down" onChange={this.handleChange}>
           <option value="top">Top</option>
           <option value="bottom">Bottom</option>
           <option value="left">Left</option>
           <option value="right">Right</option>
         </select>
-        <Button />
+
+        {/* passing position as props in Button component */}
+        <Button position={this.state.position} />
       </div>
     );
   }
